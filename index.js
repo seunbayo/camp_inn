326ac308d8cdf258e2a0171dc20403823986f0a4
seunbayo <seunbayo83@gmail.com>
2021-05-16 14:14:25 +0100

    added node packages

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('hello from the other side')
+});
+
+
+app.listen(3000, () => {
+    console.log('server is running');
+    
+})
