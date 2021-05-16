4d25599fe50d5b53ffe2ce5e0f9f3f07ac720582
seunbayo <seunbayo83@gmail.com>
2021-05-16 16:38:29 +0100

    added models

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/camp-inn', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error;"));
+db.once("open", () => {
+    console.log("database connected");
+    
+});
+
+
+
+
 const app = express();
 
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+
 app.get('/', (req, res) => {
-    res.send('hello from the other side')
+    res.render('home')
 });
 
 
 app.listen(3000, () => {
     console.log('server is running');
-    
+
 })
