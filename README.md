a1ae0eb2623846b59e8c51784c22a53da25184e9
seunbayo <seunbayo83@gmail.com>
2021-05-21 11:00:20 +0100

    fixed seeds error

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # camp_inn
+
+  for (let i = 0; i < 300; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campinn({
+
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    })
+    await camp.save();
+  }
+
+  .then(() => {
+    mongoose.connection.close();
+  });
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
--- seeds/index.js
+++ seeds/index.js
@@ -3,56 +3,35 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedhelpers');
 const Campinn = require('../models/campinn');
 
-mongoose.connect('mongodb://localhost:27017/yelp-inn', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+mongoose.connect('mongodb://localhost:27017/camp-inn', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
+
 const seedDB = async () => {
-    await Campinn.deleteMany({});
-    for (let i = 0; i < 300; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campinn({
-            //YOUR USER ID
-            author: '5f5c330c2cd79d538f2c66d9',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price,
-            geometry: {
-                type: "Point",
-                coordinates: [
-                    cities[random1000].longitude,
-                    cities[random1000].latitude,
-                ]
-            },
-            images: [
-                {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
-                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
-                },
-                {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
-                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
-                }
-            ]
-        })
-        await camp.save();
-    }
+  await Campinn.deleteMany({});
+  for (let i = 0; i < 300; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campinn({
+
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
+
 }
 
-seedDB().then(() => {
-    mongoose.connection.close();
-})
+seedDB();
